e30e488843883cee1fda8e1ccf19e80c991eaedd
Almas Khan <khanalmas753@gmail.com>
2025-09-18 22:27:02 +0200

    Jasmine

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,41 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+      it ("should return undefined if any of the argument is not a number", () => {
+    expect();
+    expect(add(5, 'hello')).toBeUndefined(undefined);
+    expect(add('5', '10')).toBeUndefined(undefined);
+    expect(add(5, undefined)).toBeUndefined(undefined);
+    expect(add(null, 10)).toBeUndefined(undefined);
+      })
     });
   });
-  
+   
+// How many tests are there for the add function in the add.spec.js file?
+// there are two add test function in the add.spec.js file. 
+
+// How are the blocks describe and it being used in the tests? What is the purpose of each?
+
+// How are the test descriptions phrased? Are there any keywords that stand out?
+
+// What do the expect functions do, and what input do they take?
+// expect functions take the value/variable and tells what to expect the when the code runs and what should be the response/solution/output.
+
+
+//  Review the Code
+
+// Review the add function (in the src/add.js file) to understand how functionality is implemented and how it relates to the tests.
+
+// Write a New Unit Test
+
+// Add a new test using the it function to test for the following requirement:
+
+// The function should return undefined if any of the two arguments is not a number.
+// To run the tests, open SpecRunner.html using the Live Share extension.
+// As per the Red phase of the Red-Green-Refactor cycle, the test should initially fail 🔴 and that is fine!
+
+
+// Write the Code to Make the Test Pass
+
+// Update the code of the add function to make the new test pass.
+// Once you correctly implement the new functionality, the test should turn green 🟢.
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,51 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
+
 
 describe("Iteration 2", () => {
     describe("Function - divide", () => {
         
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should be defined", () => {
+            expect(divide).toBeDefined();
+        });
+        it("should take two arguments", () => {
+            expect(divide.length).toBe(2);
+        });
+        it("should return the division of the two numbers", () =>{
+            expect(divide(5, 2)).toEqual(2.5);
+            expect(divide(15, 5)).toEqual(3);
+            expect(divide(5, 1)).toEqual(5);
+        });
+        it("should return undefined if any of the arguments is not provided", () => {
+            expect(divide()).toBeUndefined();
+            expect(divide(5)).toBeUndefined();
+            expect(divide(25), undefined).toBeUndefined();
+        });
 
     })    
 })
 
-*/
+// Here are the requirements for the divide function:
+
+// The function should be defined.
+// The function should take two numbers as arguments.
+// The function should return the division of the two numbers.
+// The function should return undefined if any of the arguments is not provided
+
+// Here are the steps you should follow:
+
+// Write the unit tests for the divide function in the tests/divide.spec.js file.
+//  Make sure to include at least one test for each of the requirements listed above.
+
+// You can use the unit tests for the add function as a reference.
+
+// Run the tests by opening the SpecRunner.html file using the Live Server extension. 
+// You should see that all tests are failing.
+
+// Implement the function divide function in the src/divide.js file.
+
+// Rerun the tests again. You should see that all tests are passing.
+// Once the tests are passing, review the code and refactor if needed.
+//  Some questions to ask: Are there any additional edge cases that you should test for and implement in the function?
+//  If so, go ahead and repeat the process of Red-Green-Refactor until you are satisfied with the code.
